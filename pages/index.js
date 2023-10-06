1683f42e19b16c3a692df25e65651bbd1a9c7e16
kartikmukati <kartikmukati2021@gmail.com>
2023-10-07 02:12:28 +0530

    v18 first commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-utils'
 import EventList from '../components/events/event-list';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+
+  
 
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
 }
 
 export default HomePage;
+
+export async function getStaticProps() {
+
+  const response = await fetch('https://nextjs-my-firstapp-v12-default-rtdb.firebaseio.com/sales.json')
+  console.log(response)
+
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents
+    }
+  }
+}
