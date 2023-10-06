d8cf182118b7ead07496344d31f9dbac0287fd13
kartikmukati <kartikmukati2021@gmail.com>
2023-10-06 15:42:31 +0530

    page pre-rendering project setup v18-1

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,14 @@
-import path from 'path';
-import { promises as fs} from 'fs';
-import Link from 'next/link';
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
 
-export default function Home(props) {
-
-  const { products } = props;
+function HomePage() {
+  const featuredEvents = getFeaturedEvents();
 
   return (
-  <ul>
-    {products.map((product) => (
-      <li key={product.id}><Link href={`/products/${product.id}`}><h1>{product.title}</h1></Link></li>
-    ))}
-  </ul>
-  )
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
+  );
 }
 
-export async function getStaticProps() {
-
-  const jsonDir = path.join(process.cwd() + '/data');
-  const staticData = await fs.readFile(jsonDir + '/dummy-backend.json', 'utf-8');
-  const data = JSON.parse(staticData)
-  
-  return {
-    props: {
-      products: data.products
-    },
-  }
-}
+export default HomePage;
