267c6640048b664ee1c9f80343191ab83c4a826f
kartikmukati <kartikmukati2021@gmail.com>
2023-10-28 16:45:45 +0530

    image optimizing image component in next js

--- components/event-detail/event-logistics.js
+++ components/event-detail/event-logistics.js
@@ -3,6 +3,8 @@ import DateIcon from '../icons/date-icon';
 import LogisticsItem from './logistics-item';
 import classes from './event-logistics.module.css';
 
+import Image from 'next/image'
+
 function EventLogistics(props) {
   const { date, address, image, imageAlt } = props;
 
@@ -16,7 +18,8 @@ function EventLogistics(props) {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <Image src={`/${image}`} alt={imageAlt} width={300} height={300}/>
+        {/* <img src={`/${image}`} alt={imageAlt} /> */}
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
